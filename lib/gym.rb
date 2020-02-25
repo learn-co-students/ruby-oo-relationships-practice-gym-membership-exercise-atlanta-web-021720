36253f3348be121fbbb2ff585f6761de7e40ee43
arieldavis22 <aadavisbusiness@gmail.com>
2020-02-24 21:03:35 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,42 @@
 class Gym
   attr_reader :name
 
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select do |gyms|
+      gyms.gym == self
+    end
+  end
+
+  def lifters
+    lifters_at_gym = Membership.all.select do |lifter|
+      lifter.gym == self
+    end
+    lifters_at_gym.map do |lifter|
+      lifter.lifter
+    end
+  end
+
+  def lifter_names
+    llifters.map do |name|
+      name.name
+    end
+  end
+
+  def lift_total
+    total_n = 0
+    lifters.map do |total|
+      total_n += total.lift_total
+    end
+    total_n
   end
 end
