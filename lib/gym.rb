3e29062070f8b276190d3e7f37d26c1f45f77785
gcampbell391 <gcampbell391@gmail.com>
2020-02-24 19:31:05 -0500

    Initialize git

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,31 @@
 class Gym
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def memberships
+    Membership.all.select do |membership|
+      membership.gym == self
+    end
+  end
+  def lifters
+    memberships.map do |membership|
+      membership.lifter.name
+    end
+  end
+  def lifter_names
+    memberships.map do |membership|
+      membership.lifter.name
+    end
+  end
+  def lifters_lift_total 
+    memberships.reduce(0) do |total, membership|
+      total += membership.lifter.lift_total 
+    end
   end
 end
